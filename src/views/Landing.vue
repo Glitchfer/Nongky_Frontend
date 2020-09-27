65068eb8488b28751fa1d65a2394da3e27576743
Glitchfer <arif.0496.rahman@gmail.com>
2020-09-28 00:06:13 +0700

    change landing page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="landing">
     <b-container>
-      <h1>Landing Page</h1>
+      <br />
+      <h1>Selamat datang ^__^</h1>
+      <hr />
+      <br />
+      <br />
+      <p>silahkan login dahulu!</p>
       <router-link to="/login">login</router-link>
       <hr />
-      <button type="button" @click="onLogout">logout</button>
     </b-container>
   </div>
 </template>
